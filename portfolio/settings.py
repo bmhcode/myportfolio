941835c65372967a58852979a5f87d4d81d393d7
bmhcode <51011661+bmhcode@users.noreply.github.com>
2026-02-03 00:27:49 +0100

    ALLOWED_HOSTS = ['*']

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
-
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
