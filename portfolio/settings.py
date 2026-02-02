2a418a87bcc10e4f4a2c57701c8a8df9daf92b08
bmhcode <bmhwin@gmail.com>
2026-02-02 01:27:50 +0100

    Add PostgreSQL support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,6 +82,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+DATABASES = {
+    'default': dj_database_url.config
+    (default=os.getenv('DATABASE_URL'), conn_max_age=600, conn_health_checks=True)
 }
 
 
